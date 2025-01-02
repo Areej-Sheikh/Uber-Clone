5c4d766c6077ee36eac76c413de46d8a1b3fdcc3
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-01-02 14:35:03 +0530

    JWT And Bcrypt Setup

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,10 @@ connectToDb()
 // Enable CORS
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
   fullname: {
     firstname: {
@@ -26,5 +28,19 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
 });
-const userModel = mongoose.model("user", userSchema);
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
